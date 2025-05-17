c041b0236f79e194cf096ed4f512226982f37912
otto1971 <otto.herbort1971@web.de>
2025-05-17 15:35:52 +0200

    new dev

---
+++ dbtlearn/models/tests/test_summary.sql
@@ -0,0 +1,63 @@
+{{ config(
+    materialized='incremental',
+    on_schema_change='fail'
+    )
+}}
+
+with test_summary as
+(
+select 
+    current_timestamp as time_stamp,
+    'DIM_LISTINGS_WITH_HOSTS_LISTING' as module_name,
+    'ACCEPTED_VALUES_DIM_LISTINGS_WITH_HOSTS_LISTING_ID__1__2__3' as test_name,
+    'ACCEPTED_VALUES' as test_type,
+    case when count(*)>0 then 'failed' else 'not failed' end as fail_type,
+    count(*) as faild_count
+from AIRBNB.DEV_DBT_TEST_TABLES.ACCEPTED_VALUES_DIM_LISTINGS_WITH_HOSTS_LISTING_ID__1__2__3
+union all
+select 
+    current_timestamp as time_stamp,
+    'DIM_LISTINGS_WITH_HOSTS_LISTING' as module_name,
+    'NOT_NULL_DIM_LISTINGS_WITH_HOSTS_LISTING_ID' as test_name,
+    'NOT_NULL' as test_type,
+    case when count(*)>0 then 'failed' else 'not failed' end as fail_type,
+    count(*) as faild_count
+from AIRBNB.DEV_DBT_TEST_TABLES.NOT_NULL_DIM_LISTINGS_WITH_HOSTS_LISTING_ID
+union all
+select 
+    current_timestamp as time_stamp,
+    'FCT_REVIEWS' as module_name,
+    'RELATIONSHIPS_FCT_REVIEWS' as test_name,
+    'RELATIONSHIPS' as test_type,
+    case when count(*)>0 then 'failed' else 'not failed' end as fail_type,
+    count(*) as faild_count
+from AIRBNB.DEV_DBT_TEST_TABLES.RELATIONSHIPS_FCT_REVIEWS_2F397514D7615E5AE30D8FCE9F0EA58D
+union all
+select 
+    current_timestamp as time_stamp,
+    'FCT_REVIEWS' as module_name,
+    'NOT_NULL_FCT_REVIEWS_LISTING_ID' as test_name,
+    'NOT_NULL' as test_type,
+    case when count(*)>0 then 'failed' else 'not failed' end as fail_type,
+    count(*) as faild_count
+from AIRBNB.DEV_DBT_TEST_TABLES.NOT_NULL_FCT_REVIEWS_LISTING_ID
+union all
+select 
+    current_timestamp as time_stamp,
+    'DIM_LISTINGS' as module_name,
+    'UNIQUE_DIM_LISTINGS_WITH_HOSTS_LISTING_ID' as test_name,
+    'NOT_NULL' as test_type,
+    case when count(*)>0 then 'failed' else 'not failed' end as fail_type,
+    count(*) as faild_count
+from AIRBNB.DEV_DBT_TEST_TABLES.UNIQUE_DIM_LISTINGS_WITH_HOSTS_LISTING_ID
+union all
+select 
+    current_timestamp as time_stamp,
+    'FCT_REVIEWS' as module_name,
+    'UNIQUE_FCT_REVIEWS_LISTING_ID' as test_name,
+    'NOT_NULL' as test_type,
+    case when count(*)>0 then 'failed' else 'not failed' end as fail_type,
+    count(*) as faild_count
+from AIRBNB.DEV_DBT_TEST_TABLES.UNIQUE_FCT_REVIEWS_LISTING_ID
+)
+select * from test_summary;
