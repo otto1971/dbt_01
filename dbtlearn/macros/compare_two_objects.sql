12814809ef99b3227a0ae94e07017c1313732a78
otto1971 <otto.herbort1971@web.de>
2025-05-17 10:56:34 +0200

    compare_two_objects und schema.yml

---
+++ dbtlearn/macros/compare_two_objects.sql
@@ -0,0 +1,22 @@
+{% test compare_two_objects(target_model, source_model, target_column, source_column) %}
+
+with target_table as (
+    select cast({{target_column}} as char(40)) as target_column
+    from {{target_model}} 
+),
+source_table as (
+    select cast({{source_column}} as char(40)) as source_column
+    from {{source_model}} 
+),
+finalresult as
+(
+select target_column as result_data
+from target_table 
+where target_column not in 
+    (
+    select source_column from source_table
+    )
+)
+select count(result_data) from finalresult
+
+{% endtest %}
